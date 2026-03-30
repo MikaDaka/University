e5f84b877048aed099550172c936a66cc091c32b
MikaDaka <150288147+MikaDaka@users.noreply.github.com>
2026-03-30 12:07:30 +0300

    Delete ПИС/Весь Django-проект directory

--- ПИС/Весь Django-проект/lab8_jquery/script.js
+++
@@ -1,9 +0,0 @@
-$(".post").hover(
-    function() { $(this).css("background", "#e0f0ff"); },
-    function() { $(this).css("background", "white"); }
-);
-
-$("#logo").hover(
-    function() { $(this).css("transform", "rotate(10deg)"); },
-    function() { $(this).css("transform", "rotate(0deg)"); }
-);
